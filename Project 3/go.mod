51814a68b7f06ae6e964190f034fe9b8167e998c
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2022-05-30 01:03:47 +0530

    Project 3

---
+++ Project 3/go.mod
@@ -0,0 +1,11 @@
+module github.com/AMANKANOJIYA/Golang-Projects/tree/main/Project3
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.23.5 // indirect
+)
---
+++ Project 3/pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controllers "github.com/AMANKANOJIYA/Golang-Projects/tree/main/Project3/pkg/controller"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}

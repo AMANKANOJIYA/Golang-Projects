aec5ad836e0860948079a65ebecd9d3ac879192f
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2022-06-03 23:04:12 +0530

    Domain Verification

---
+++ Project 4/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("This is a Email Domain Verification ( Past your Verification Email Below ) \n")
+	fmt.Printf(": ---- > ")
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error Could not read from input : %v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error : %v\n", err)
+	}
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error : %v\n", err)
+	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error : %v\n", err)
+	}
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Println("\n=================================================")
+	fmt.Println("\nDomain : ", domain)
+	fmt.Println("\n------------------------------------------------")
+	fmt.Println("\nHas MX : ", hasMX)
+	fmt.Println("\n------------------------------------------------")
+	fmt.Println("\nHas SPF : ", hasSPF)
+	fmt.Println("\n------------------------------------------------")
+	fmt.Println("\nSPF Records : ", spfRecord)
+	fmt.Println("\n------------------------------------------------")
+	fmt.Println("\nHas DMARC : ", hasDMARC)
+	fmt.Println("\n------------------------------------------------")
+	fmt.Println("\nDMARC Record : ", dmarcRecord)
+	fmt.Println("\n=================================================")
+}
